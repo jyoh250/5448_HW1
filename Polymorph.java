578853d32fbe2e7affb84635bc73c74d92db4e6c
jyoh250 <46973086+jyoh250@users.noreply.github.com>
2019-01-25 15:11:17 -0700

    Update Polymorph.java

--- Polymorph.java
+++ Polymorph.java
@@ -14,12 +14,15 @@ class Polymorph {
       System.out.println("==Select shape==");
       System.out.println("1.rectangle");
       System.out.println("2.circle");
+      System.out.println("3.Exit");
       choice = keyboard.nextInt();
 
       if(choice == 1){
          shapeO[0].draw();
       }else if(choice ==2){
          shapeO[1].draw();
+      }else if(choice == 3){
+         System.exit(0);
       }else{
          System.out.println("Please type number 1 or 2.");
       }
